ad209dd2997cd2e2b068ae4f7dbfddde7742de75
Toby Clemson <tobyclemson@gmail.com>
2018-06-12 15:47:28 +0100

    Validate options in Fly.

--- src/Fly.js
+++ src/Fly.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Joi from 'joi';
 import { Base64 } from 'js-base64';
 
 const authorizationHeaderName = 'Authorization';
@@ -23,24 +24,62 @@ const authTokenUri = (uri, team) =>
 const jobsUri = (uri, team, pipeline) =>
   `${uri}/teams/${team}/pipelines/${pipeline}/jobs`;
 
+const string = () => Joi.string();
+const uri = () => string().uri();
+
+const schemaFor = (config) =>
+  Joi.object().keys(config);
+
+const validateOptions = (schema, options) => {
+  const { error, value } = schema.validate(options);
+
+  if (error) {
+    const errorDetails = error.details
+      .map(detail => detail.message)
+      .join(', ');
+    throw new Error(`Invalid parameter(s): [${errorDetails}].`);
+  }
+
+  return value;
+};
+
 export default class Fly {
   constructor(options) {
-    this.uri = options.uri;
-    this.team = options.team;
-    this.username = options.username;
-    this.password = options.password;
+    const validatedOptions = validateOptions(
+      schemaFor({
+        uri: uri().required(),
+        team: string(),
+        username: string(),
+        password: string(),
+      }), options);
+
+    this.uri = validatedOptions.uri;
+    this.team = validatedOptions.team;
+    this.username = validatedOptions.username;
+    this.password = validatedOptions.password;
   }
 
   async login(options) {
+    const validatedOptions = validateOptions(
+      schemaFor({
+        username: string().required(),
+        password: string().required(),
+      }), options);
+
     return new Fly({
       uri: this.uri,
-      team: options.team || this.team,
-      username: options.username,
-      password: options.password,
+      team: validatedOptions.team || this.team,
+      username: validatedOptions.username,
+      password: validatedOptions.password,
     });
   }
 
   async jobs(options) {
+    const validatedOptions = validateOptions(
+      schemaFor({
+        pipeline: string().required()
+      }), options);
+
     const bearerAuthTokenResponse = await axios
       .get(authTokenUri(this.uri, this.team), {
         headers: basicAuthHeader(this.username, this.password)
@@ -48,7 +87,7 @@ export default class Fly {
     const bearerAuthToken = bearerAuthTokenResponse.data;
 
     const jobsResponse = await axios
-      .get(jobsUri(this.uri, this.team, options.pipeline), {
+      .get(jobsUri(this.uri, this.team, validatedOptions.pipeline), {
         headers: bearerAuthHeader(bearerAuthToken.value)
       });
     const jobs = jobsResponse.data;
--- test/Fly.spec.js
+++ test/Fly.spec.js
@@ -7,6 +7,13 @@ import { buildAuthToken, buildJob } from './builders';
 
 import Fly from '../src/Fly';
 
+const newFly = ({
+  uri = faker.internet.url(),
+  team = 'main',
+  username = 'some-username',
+  password = 'some-password',
+} = {}) => new Fly({ uri, team, username, password });
+
 describe('Fly', () => {
   const mock = new MockAdapter(axios);
 
@@ -18,7 +25,143 @@ describe('Fly', () => {
     mock.restore();
   });
 
+  describe('construction', () => {
+    it('throws an exception if the URI is not provided', () => {
+      expect(() => {
+        new Fly({})
+      })
+        .to.throw(Error, 'Invalid parameter(s): ["uri" is required].');
+    });
+
+    it('throws an exception if the URI is not a string', () => {
+      expect(() => {
+        new Fly({ uri: 25 })
+      })
+        .to.throw(Error, 'Invalid parameter(s): ["uri" must be a string].');
+    });
+
+    it('throws an exception if the URI is not a valid URI', () => {
+      expect(() => {
+        new Fly({ uri: 'spinach' })
+      })
+        .to.throw(Error, 'Invalid parameter(s): ["uri" must be a valid uri].');
+    });
+
+    it('throws an exception if the provided team is not a string', () => {
+      expect(
+        () => {
+          new Fly({
+            uri: faker.internet.url(),
+            team: 35
+          });
+        })
+        .to.throw(Error, 'Invalid parameter(s): ["team" must be a string].')
+    });
+
+    it('throws an exception if the provided username is not a string', () => {
+      expect(
+        () => {
+          new Fly({
+            uri: faker.internet.url(),
+            username: 35
+          });
+        })
+        .to.throw(Error, 'Invalid parameter(s): ["username" must be a string].')
+    });
+
+    it('throws an exception if the provided password is not a string', () => {
+      expect(
+        () => {
+          new Fly({
+            uri: faker.internet.url(),
+            password: 35
+          });
+        })
+        .to.throw(Error, 'Invalid parameter(s): ["password" must be a string].')
+    });
+  });
+
+  describe('login', () => {
+    it('throws an exception if the username is not provided', async () => {
+      try {
+        await newFly()
+          .login({ password: 'some-password' });
+      } catch(e) {
+        expect(e instanceof Error).to.eql(true);
+        expect(e.message)
+          .to.eql('Invalid parameter(s): ["username" is required].');
+        return
+      }
+      expect.fail(null, null, 'Expected exception but none was thrown.');
+    });
+
+    it('throws an exception if the provided username is not a string', async () => {
+      try {
+        await newFly()
+          .login({ username: 25, password: 'some-password' });
+      } catch(e) {
+        expect(e instanceof Error).to.eql(true);
+        expect(e.message)
+          .to.eql('Invalid parameter(s): ["username" must be a string].');
+        return
+      }
+      expect.fail(null, null, 'Expected exception but none was thrown.');
+    });
+
+    it('throws an exception if the password is not provided', async () => {
+      try {
+        await newFly()
+          .login({ username: 'some-username' });
+      } catch(e) {
+        expect(e instanceof Error).to.eql(true);
+        expect(e.message)
+          .to.eql('Invalid parameter(s): ["password" is required].');
+        return
+      }
+      expect.fail(null, null, 'Expected exception but none was thrown.');
+    });
+
+    it('throws an exception if the provided password is not a string', async () => {
+      try {
+        await newFly()
+          .login({ username: 'some-username', password: 25 });
+      } catch(e) {
+        expect(e instanceof Error).to.eql(true);
+        expect(e.message)
+          .to.eql('Invalid parameter(s): ["password" must be a string].');
+        return
+      }
+      expect.fail(null, null, 'Expected exception but none was thrown.');
+    });
+
+    // team override
+  });
+
   describe('jobs', () => {
+    it('throws an exception if no pipeline is provided', async () => {
+      try {
+        await newFly().jobs({});
+      } catch(e) {
+        expect(e instanceof Error).to.eql(true);
+        expect(e.message)
+          .to.eql('Invalid parameter(s): ["pipeline" is required].');
+        return
+      }
+      expect.fail(null, null, 'Expected exception but none was thrown.');
+    });
+
+    it('throws an exception if the provided pipeline is not a string', async () => {
+      try {
+        await newFly().jobs({ pipeline: 25 });
+      } catch(e) {
+        expect(e instanceof Error).to.eql(true);
+        expect(e.message)
+          .to.eql('Invalid parameter(s): ["pipeline" must be a string].');
+        return
+      }
+      expect.fail(null, null, 'Expected exception but none was thrown.');
+    });
+
     it('gets all jobs in the supplied pipeline', async () => {
       const uri = 'https://concourse.example.com';
 
