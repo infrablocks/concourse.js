74412c8776c4b770f59035314401639b464552aa
Toby Clemson <tobyclemson@gmail.com>
2022-01-17 14:32:53 +0000

    Use correct node installation instructions.

--- scripts/ci/common/install-node-and-npm.sh
+++ scripts/ci/common/install-node-and-npm.sh
@@ -4,10 +4,13 @@
 set -e
 set -o pipefail
 
-NODE_VERSION=14.18.3
+# inspired by:
+# https://github.com/nodejs/docker-node/blob/85ca3893867505ffbffbdf476722d3897fb3da98/14/buster/Dockerfile
+NODE_VERSION 14.18.3
 
 ARCH=
 dpkgArch="$(dpkg --print-architecture)"
+
 case "${dpkgArch##*-}" in
   amd64) ARCH='x64';;
   ppc64el) ARCH='ppc64le';;
@@ -18,10 +21,12 @@ case "${dpkgArch##*-}" in
   *) echo "unsupported architecture"; exit 1 ;;
 esac
 
+# gpg keys listed at https://github.com/nodejs/node#release-keys
+set -ex
 for key in \
     4ED778F539E3634C779C87C6D7062848A1AB005C \
     94AE36675C464D64BAFA68DD7434390BDBE9B9C5 \
-    1C050899334244A8AF75E53792EF661D867B9DFA \
+    74F12602B6F1C4E913FAA37AD3A89613643B6201 \
     71DCFD284A79C3B38668286BC97EC7A07EDE3FC1 \
     8FCCA13FEF1D0C2E91008E09770F7A9A5AE15600 \
     C4F0DFFF4E8C1A8236409D08E73BC641CC11F4C8 \
@@ -31,9 +36,8 @@ for key in \
     108F52B48DB57BB0CC439B2997B01419BD92F80A \
     B9E2F5981AA6E0CD28160D9FF13993A75599653C \
 ; do
-  gpg --batch --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys "$key" || \
-  gpg --batch --keyserver hkp://ipv4.pool.sks-keyservers.net --recv-keys "$key" || \
-  gpg --batch --keyserver hkp://pgp.mit.edu:80 --recv-keys "$key"
+  gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys "$key" || \
+  gpg --batch --keyserver keyserver.ubuntu.com --recv-keys "$key" ;
 done
 
 curl -fsSLO --compressed "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-$ARCH.tar.xz"
